c7daa84610faa81fa5a2882609f855ba7629b8b1
Simon Litchfield <simon@s29.com.au>
2014-05-14 18:03:55 +1000

    Improved handling of empty values

--- multiselectfield/db/fields.py
+++ multiselectfield/db/fields.py
@@ -119,7 +119,9 @@ class MultiSelectField(models.CharField):
                             item_display = choicedict.get(int(value), value)
                         except (ValueError, TypeError):
                             item_display = value
-                    display.append(string_type(item_display))
+                    item_display = string_type(item_display)
+                    if item_display not in ('', u'', None):
+                        display.append(item_display)
                 return display
             def get_display(obj):
                 return ", ".join(get_list(obj))
