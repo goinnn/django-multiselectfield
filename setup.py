212a833e10f9378eb9d3d28a66528707f5eeb108
Danilo G. Botelho <danilogbotelho@yahoo.com>
2015-06-03 15:40:28 -0300

    next release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name="django-multiselectfield",
-    version="0.1.3",
+    version="0.1.4",
     author="Pablo Martin",
     author_email="goinnn@gmail.com",
     description="Django multiple select field",
