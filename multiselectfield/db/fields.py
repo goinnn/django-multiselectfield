5eb16b7ee8a7206eec8ac50e651920a4a2b6c743
Drew Hubl <drew.hubl@gmail.com>
2017-01-01 19:21:22 -0700

    Fix min_choices commit

--- multiselectfield/db/fields.py
+++ multiselectfield/db/fields.py
@@ -50,6 +50,7 @@ class MultiSelectField(models.CharField):
     """ Choice values can not contain commas. """
 
     def __init__(self, *args, **kwargs):
+        self.min_choices = kwargs.pop('min_choices', None)
         self.max_choices = kwargs.pop('max_choices', None)
         super(MultiSelectField, self).__init__(*args, **kwargs)
         self.max_length = get_max_length(self.choices, self.max_length)
