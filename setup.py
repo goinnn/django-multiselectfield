e81ad8106de557de25bd15192ada45e68d784fea
Drew Hubl <drew.hubl@gmail.com>
2017-05-18 07:04:25 -0600

    Bump version to 0.1.7 for PyPI release

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name="django-multiselectfield",
-    version="0.1.6",
+    version="0.1.7",
     author="Pablo Martin",
     author_email="goinnn@gmail.com",
     description="Django multiple select field",
