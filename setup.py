af939b05fb1bdbb95befcbc88ec86a89cedd58f0
Drew Hubl <drew.hubl@gmail.com>
2017-05-18 07:02:18 -0600

    Add Python 3.6 to the PyPI classifiers section

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     license="LGPL 3",
     keywords="django,multiple,select,field,choices",
