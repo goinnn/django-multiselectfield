a0b438280ca2f21e973c2b56e9e0ff6e0a15113d
Chihiro Kaneko <chihiro.kaneko@uncovertruth.co.jp>
2018-09-21 18:47:57 +0900

    Fix deprecation warnings.

--- multiselectfield/db/fields.py
+++ multiselectfield/db/fields.py
@@ -147,10 +147,16 @@ class MultiSelectField(models.CharField):
             return value if isinstance(value, list) else MSFList(choices, value.split(','))
         return MSFList(choices, [])
 
-    def from_db_value(self, value, expression, connection, context):
-        if value is None:
-            return value
-        return self.to_python(value)
+    if VERSION < (2, ):
+        def from_db_value(self, value, expression, connection, context):
+            if value is None:
+                return value
+            return self.to_python(value)
+    else:
+        def from_db_value(self, value, expression, connection):
+            if value is None:
+                return value
+            return self.to_python(value)
 
     def contribute_to_class(self, cls, name):
         super(MultiSelectField, self).contribute_to_class(cls, name)
