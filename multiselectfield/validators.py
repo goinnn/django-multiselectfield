e57177cc3dba8a51ac2a72ae8a91b43d4fa5949a
Thijs Boehme <thijsboehme@me.com>
2019-12-12 18:03:03 +0100

    Update lazy translation import
    `ugettext_lazy` is deprecated as of Django 3.0 (to be removed in Django 4.0)

--- multiselectfield/validators.py
+++ multiselectfield/validators.py
@@ -16,7 +16,7 @@
 
 
 from django.core import validators
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class MaxValueMultiFieldValidator(validators.MaxLengthValidator):
