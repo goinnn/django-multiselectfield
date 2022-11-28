d7d92dc62ae571e0a34a2ec417fa8414ebe44db5
László Károlyi <laszlo@karolyi.hu>
2022-11-28 21:26:04 +0100

    Fix when selecting with an MSFList

--- multiselectfield/utils.py
+++ multiselectfield/utils.py
@@ -14,6 +14,30 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this programe.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
+import sys
+from collections import UserList
+from typing import Optional, Union
+
+from django.db.models.sql.query import Query
+
+if sys.version_info[0] == 2:
+    string = basestring  # noqa: F821
+    string_type = unicode  # noqa: F821
+else:
+    string = str
+    string_type = string
+
+class _FakeSqlVal(UserList):
+
+    contains_aggregate = False
+    contains_column_references = False
+    contains_over_clause = False
+
+    def __str__(self):
+        return ','.join(map(str, self))
+
 
 class MSFList(list):
 
@@ -24,6 +48,20 @@ class MSFList(list):
     def __str__(msgl):
         msg_list = [msgl.choices.get(int(i)) if i.isdigit() else msgl.choices.get(i) for i in msgl]
         return ', '.join([str(s) for s in msg_list])
+
+    def resolve_expression(
+            self, query: Query = None, allow_joins: bool = True,
+            reuse: Optional[bool] = None, summarize: bool = False,
+            for_save: bool = False) -> Union[list, _FakeSqlVal]:
+        if for_save:
+            result = _FakeSqlVal(self)
+        else:
+            result = list(self)
+        return result
+
+    if sys.version_info < (3,):
+        def __unicode__(self, msgl):
+            return self.__str__(msgl)
 
 
 def get_max_length(choices, max_length, default=200):
