2a98c3b68fb6c4c88f3c5c2ac9d2e4d938541db4
Mike Manger <mike.manger@lighthouseuk.net>
2024-07-26 13:13:56 +0100

    Replace http:// with https:// where relavent and fix a doc link

--- example/app/admin.py
+++ example/app/admin.py
@@ -12,7 +12,7 @@
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# along with this software.  If not, see <https://www.gnu.org/licenses/>.
 
 from django.contrib import admin
 
--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this programe.  If not, see <http://www.gnu.org/licenses/>.
+# along with this programe.  If not, see <https://www.gnu.org/licenses/>.
 
-# Initial code got from http://djangosnippets.org/users/danielroseman/
+# Initial code got from https://djangosnippets.org/users/danielroseman/
 
 import codecs
 import os
