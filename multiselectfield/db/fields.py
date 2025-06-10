2822a1399cb256ed260b34adb6c4a36d03ee65d7
Pablo Martín <goinnn@gmail.com>
2025-06-10 12:28:09 +0200

    Remove unnecesary code. (old code)

--- multiselectfield/db/fields.py
+++ multiselectfield/db/fields.py
@@ -25,18 +25,6 @@ from ..utils import get_max_length
 from ..validators import MaxValueMultiFieldValidator
 
 
-def add_metaclass(metaclass):
-    """Class decorator for creating a class with a metaclass."""
-    def wrapper(cls):
-        orig_vars = cls.__dict__.copy()
-        orig_vars.pop('__dict__', None)
-        orig_vars.pop('__weakref__', None)
-        for slots_var in orig_vars.get('__slots__', ()):
-            orig_vars.pop(slots_var)
-        return metaclass(cls.__name__, cls.__bases__, orig_vars)
-    return wrapper
-
-
 class MultiSelectField(models.CharField):
     """ Choice values can not contain commas. """
 
