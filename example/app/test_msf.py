6fc80aaa3c10464350490ad620787a8041345f1b
Pablo Martín <goinnn@gmail.com>
2025-06-10 12:31:55 +0200

    Move code

--- example/app/test_msf.py
+++ example/app/test_msf.py
@@ -304,3 +304,19 @@ class SortedMultiSelectTestCase(TestCase):
             tag_index = form_html.index(f'value="{tag}"')
             self.assertGreater(tag_index, previous_tag_index)
             previous_tag_index = tag_index
+
+
+class MsfSelectTestCase(TestCase):
+    fixtures = ['app_data.json']
+
+    def test_valid_select(self):
+        """
+        Should be able to use a multiselectfield result to select
+        See
+        https://github.com/goinnn/django-multiselectfield/pull/135
+        """
+        book = Book.objects.first()
+        result = Book.objects.filter(categories=book.categories).only('pk')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].pk, book.pk)
+        self.assertIn(member='1,3,5', container=str(result.query))
